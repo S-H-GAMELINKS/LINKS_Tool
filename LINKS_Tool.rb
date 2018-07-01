d50b807ca56ecc13b74e542114be6091a4ae6914
S-H-GAMELINKS <gamelinks007@gmail.com>
2018-07-01 16:24:53 +0900

    using menu var & each method

--- LINKS_Tool.rb
+++ LINKS_Tool.rb
@@ -10,16 +10,16 @@ flag = 0
 #読込変数strの定義
 str = String.new
 
+menu =["使用する機能を選択してください\n", "1: LINKS用タグ登録\n", "2:ティラノスクリプト用タグ登録\n",
+		"3:吉里吉里2用タグ登録\n", "4:Nscripter2用タグ登録\n", "5:終了\n", "使用する機能は？："]
+
 #使用する機能の選択
 while flag == 0
 
-	puts "使用する機能を選択してください"
-	puts "1: LINKS用タグ登録"
-	puts "2:ティラノスクリプト用タグ登録"
-	puts "3:吉里吉里2用タグ登録"
-	puts "4:Nscripter2用タグ登録"
-	puts "5:終了"
-	print "使用する機能は？："
+	menu.each do |menu|
+		puts menu
+	end
+
 	flag = gets.to_i
 end
 
